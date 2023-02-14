d8db9eb6fb38147fce52e9c69748988eb89044ef
Alok Kumar <y.alok51053@gmail.com>
2023-02-15 01:16:51 +0530

    url changed

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -17,7 +17,7 @@ function App() {
 
   const fetchBotResponse = async () => {
     const { data } = await axios.post(
-      "http://localhost:4000/",
+      "https://chatgpt-app-q5my.onrender.com",
       // "http://localhost:5173",
       { input },
       {
